1a5ce05ba94dead6c2bf74ba98af07501fa6ddbc
mizue-ohno <zme02.kkp33@gmail.com>
2023-06-24 14:44:00 +0900

    Controllerの作成

--- database/migrations/2023_06_24_140711_create_memos_table.php
+++ database/migrations/2023_06_24_140711_create_memos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateItemsTable extends Migration
+class CreateMemosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,19 +13,19 @@ class CreateItemsTable extends Migration
      */
     public function up()
     {
-        Schema::create('items', function (Blueprint $table) {
+        Schema::create('memos', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id');
             $table->string('name', 100)->nullable();
             $table->string('type', 16);
             $table->string('detail', 500)->nullable();
             $table->longText('image', 500)->nullable();
-            $table->date('buy_date', 500);
             $table->string('color', 16);
             $table->string('season', 16);
             $table->string('brand', 16)->nullable();
-            $table->string('group', 16)->nullable();
+            $table->integer('price')->nullable();
             $table->timestamps();
+
         });
     }
 
@@ -36,6 +36,6 @@ class CreateItemsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('items');
+        Schema::dropIfExists('memos');
     }
 }
