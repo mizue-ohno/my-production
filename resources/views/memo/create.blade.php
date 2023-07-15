466c421b95f08d54a2a6a2b12552544579cd41db
mizue-ohno <zme02.kkp33@gmail.com>
2023-07-15 12:37:04 +0900

    create のoldヘルパー

--- resources/views/memo/create.blade.php
+++ resources/views/memo/create.blade.php
@@ -29,17 +29,17 @@
                         <div class="text-red">※必須項目です。</div>
                         <select class="form-select" type="number" id="type" name="type" aria-label="Default select example">
                             <option selected>選択してください</option>
-                            <option value="トップス">トップス</option>
-                            <option value="パンツ">パンツ</option>
-                            <option value="スカート">スカート</option>
-                            <option value="ワンピース">ワンピース</option>
-                            <option value="小物">小物</option>
+                            <option value="トップス" @if( old('type')) == "トップス") selected  @endif>トップス</option>
+                            <option value="パンツ" @if( old('type')) == "パンツ") selected  @endif>パンツ</option>
+                            <option value="スカート" @if( old('type')) == "スカート") selected  @endif>スカート</option>
+                            <option value="ワンピース" @if( old('type')) == "ワンピース") selected  @endif>ワンピース</option>
+                            <option value="小物" @if( old('type')) == "小物") selected  @endif>小物</option>
                         </select>
                     </div>
 
                     <div class="form-group">
                         <label for="detail">詳細</label>
-                        <textarea class="form-control" id="detail" name="detail" placeholder="迷ったポイント、着たいシチュエーションetc."></textarea>
+                        <textarea class="form-control" id="detail" name="detail" placeholder="迷ったポイント、着たいシチュエーションetc.">{{ old('detail') }}</textarea>
                     </div>
 
                     <div class="form-group">
@@ -50,33 +50,33 @@
                     <div class="form-group">
                         <label for="color">カラー</label><br>
                         <div class="text-red">※必須項目です。</div>
-                        <input type="radio" name="color" value="白">白　
-                        <input type="radio" name="color" value="黒">黒　
-                        <input type="radio" name="color" value="赤">赤　
-                        <input type="radio" name="color" value="青">青　
-                        <input type="radio" name="color" value="黄">黄　
-                        <input type="radio" name="color" value="緑">緑　
+                        <input type="radio" name="color" value="白" {{ old('color','白') == '白' ? 'checked' : '' }}>白　
+                        <input type="radio" name="color" value="黒" {{ old('color','黒') == '黒' ? 'checked' : '' }}>黒　
+                        <input type="radio" name="color" value="赤" {{ old('color','赤') == '赤' ? 'checked' : '' }}>赤　
+                        <input type="radio" name="color" value="青" {{ old('color','青') == '青' ? 'checked' : '' }}>青　
+                        <input type="radio" name="color" value="黄" {{ old('color','黄') == '黄' ? 'checked' : '' }}>黄　
+                        <input type="radio" name="color" value="緑" {{ old('color','緑') == '緑' ? 'checked' : '' }}>緑
                     </div>
 
 
                     <div class="form-group">
                         <label for="season">着用シーズン</label><br>
                         <div class="text-red">※必須項目です。</div>
-                        <input type="radio" name="season" value="春">春　
-                        <input type="radio" name="season" value="夏">夏　
-                        <input type="radio" name="season" value="秋">秋　
-                        <input type="radio" name="season" value="冬">冬　
-                        <input type="radio" name="season" value="通年">通年　
+                        <input type="radio" name="season" value="春" {{ old('color','春') == '春' ? 'checked' : '' }}>春　
+                        <input type="radio" name="season" value="夏" {{ old('color','夏') == '夏' ? 'checked' : '' }}>夏　
+                        <input type="radio" name="season" value="秋" {{ old('color','秋') == '秋' ? 'checked' : '' }}>秋　
+                        <input type="radio" name="season" value="冬" {{ old('color','冬') == '冬' ? 'checked' : '' }}>冬　
+                        <input type="radio" name="season" value="通年" {{ old('color','通年') == '通年' ? 'checked' : '' }}>通年　
                     </div>
 
                     <div class="form-group">
                         <label for="brand">ブランド</label>
-                        <input type="text" class="form-control" id="brand" name="brand" placeholder="ブランド、売っていた場所、webサイトetc.">
+                        <input type="text" class="form-control" id="brand" name="brand" placeholder="ブランド、売っていた場所、webサイトetc." value="{{ old('brand') }}">
                     </div>
 
                     <div class="form-group">
                         <label for="price">価格</label>
-                        <input type="text" class="form-control" id="price" name="price" placeholder="半角数字で入力してください">
+                        <input type="text" class="form-control" id="price" name="price" placeholder="半角数字で入力してください" value="{{ old('price') }}">
                     </div>
 
                     <div class="card-footer">
