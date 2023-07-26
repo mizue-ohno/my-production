c6f86776e956bcfaeff3dd5c5752d50bc7762ce7
mizue-ohno <zme02.kkp33@gmail.com>
2023-07-26 13:25:01 +0900

    アイコンの変更

--- config/adminlte.php
+++ config/adminlte.php
@@ -249,13 +249,13 @@ return [
         [
             'text' => '(管理)アイテムリスト',
             'url'  => 'items/admin',
-            'icon' => 'fas fa-tshirt',
+            'icon' => 'far fa-tshirt',
             'can' => 'admin',
         ],
         [
             'text' => '(管理)欲しいものリスト',
             'url'  => 'memos/admin',
-            'icon' => 'fas fa-heart',
+            'icon' => 'far fa-heart',
             'can' => 'admin',
         ],
 
